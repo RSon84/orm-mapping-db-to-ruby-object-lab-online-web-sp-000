13da07d71b98825082a579b4449d1229db631c8c
Devin Wilson <craigvin84@yahoo.com>
2021-05-17 20:40:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,9 @@ class Student
     FROM students
   SQL
 
-  DB[:conn].execute(sql)
+  #DB[:conn].execute(sql)
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
   end
 
   def self.find_by_name(name)
