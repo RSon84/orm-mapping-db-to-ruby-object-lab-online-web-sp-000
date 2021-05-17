c0b7f7d2458c6c347edb9c079ad315731af3ee2b
Devin Wilson <craigvin84@yahoo.com>
2021-05-17 20:41:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,7 @@ class Student
   DB[:conn].execute(sql).collect do |row|
     self.new_from_db(row)
   end
+  end
 
   def self.find_by_name(name)
     # find the student in the database given a name
