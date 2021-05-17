a40051bb008aeed99e522b84712cac1e07a7ad02
Devin Wilson <craigvin84@yahoo.com>
2021-05-17 20:46:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,9 @@ class Student
      SELECT * FROM students WHERE grade = 10 LIMIT ?
   SQL
 
-  DB[:conn].execute(sql, x)
-
+  DB[:conn].execute(sql, x)collect do |row|
+    self.new_from_db(row)
+end
 end
 
   def save
